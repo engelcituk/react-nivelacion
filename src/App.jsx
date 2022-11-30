62f1e35311f35b0600e867b7f60b3db43c9b74dd
eCituk <ecituk@thefives.com.mx>
2022-11-30 17:21:35 -0500

    Lazy - Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { lazy, Suspense } from "react"
 import Counter from "./components/Counter"
-import Users from "./components/Users"
-
 import "./App.css"
 
+
+
+const Users = lazy(() => import("./components/Users") )
+
 const App = () => {
-  return <div className="App-header">
-      <Counter/>
-      <Users/>
-  </div>
+  return(
+    <div className="App-header">
+        <Counter/>
+        <Suspense fallback={<div>Cargando..</div>}>
+          <Users/>
+        </Suspense>
+    </div>
+    )
    
 }
 
