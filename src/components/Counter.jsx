19d1aa0679754a61428f5a1710a5c2e1a9264c50
cituk <ecituk@thefives.com.mx>
2022-11-30 00:05:20 -0500

    Styled components

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,18 @@
+import styled from "@emotion/styled"
 import {  useState } from "react"
+
 import "./Counter.css"
 
+const BtnDiminish = styled.button`
+    color: white;
+    padding: 1rem;
+    background-color: #4e1313;
+    border: none;
+    &:hover{
+        background-color:#812121 ;
+        font-weight: bold;
+    }
+`
 const Counter = ()=> {
 
     const [counter, setCounter] = useState(5)
@@ -8,14 +20,21 @@ const Counter = ()=> {
     const handleClick = () => {
         setCounter(counter + 1)
     }
+    const handleDiminish = () => {
+        setCounter(counter - 1)
+    }
+
     return(
         <div>
             <h1>Counter: {counter}</h1>
             <button onClick={ handleClick } style={{
                 padding: "1rem",
                 backgroundColor: "#739122",
-                border: "none"
+                border: "none",
+                marginRight: "5px"
             }}>Add</button>
+
+            <BtnDiminish onClick={handleDiminish}>Diminish</BtnDiminish>
         </div>
     )
 }
